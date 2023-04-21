ebd2de07cd6b496727a48e25491f03a7f3152e1d
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2023-04-21 14:56:51 +0200

    updated index.js imported cors

--- config/index.js
+++ config/index.js
@@ -1,3 +1,6 @@
+const cors = require('cors');
+
+
 // Middleware configuration
 module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted on a server with a `proxy`, express needs to know that it should trust that setting.
