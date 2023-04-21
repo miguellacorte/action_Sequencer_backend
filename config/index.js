f19e2561216ffae48a80a660f010e07c6b924708
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2023-04-21 14:31:26 +0200

    updated index.js

--- config/index.js
+++ config/index.js
@@ -1,21 +1,6 @@
-// We reuse this import in order to have access to the `body` property in requests
-const express = require("express");
-
-// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
-// https://www.npmjs.com/package/morgan
-const logger = require("morgan");
-
-// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
-// https://www.npmjs.com/package/cookie-parser
-const cookieParser = require("cookie-parser");
-
-// ℹ️ Needed to accept from requests from 'the outside'. CORS stands for cross origin resource sharing
-// unless the request if from the same domain, by default express wont accept POST requests
-const cors = require("cors");
-
 // Middleware configuration
 module.exports = (app) => {
-  // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
+  // Because this is a server that will accept requests from outside and it will be hosted on a server with a `proxy`, express needs to know that it should trust that setting.
   // Services like heroku use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
 
@@ -23,7 +8,7 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: [process.env.ORIGIN || "http://localhost:3000", "https://action-sequencer.onrender.com/"],
+      origin: [process.env.ORIGIN || "http://localhost:3000", "https://action-sequencer.onrender.com"],
     })
   );
 
@@ -34,4 +19,15 @@ module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+
+  // Set the CORS headers for each response
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.ORIGIN || 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
+  // Your API routes go here
 };
