c906a7ab65f99282ff01a4a01d77e24a2b85c9fd
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2023-04-21 15:07:52 +0200

    corrected index.js

--- config/index.js
+++ config/index.js
@@ -1,5 +1,17 @@
-const cors = require('cors');
+// We reuse this import in order to have access to the `body` property in requests
+const express = require("express");
 
+// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
+// https://www.npmjs.com/package/morgan
+const logger = require("morgan");
+
+// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
+// https://www.npmjs.com/package/cookie-parser
+const cookieParser = require("cookie-parser");
+
+// ℹ️ Needed to accept from requests from 'the outside'. CORS stands for cross origin resource sharing
+// unless the request if from the same domain, by default express wont accept POST requests
+const cors = require("cors");
 
 // Middleware configuration
 module.exports = (app) => {
