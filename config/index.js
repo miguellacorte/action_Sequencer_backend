cb9c8e501f2c7f7f5bd9ca18179419cf6772401a
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2023-04-21 15:40:02 +0200

    updated cors

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: [process.env.ORIGIN || "http://localhost:3000", "https://action-sequencer.onrender.com"],
+      origin: [process.env.ORIGIN || "http://localhost:3000/", "https://action-sequencer.onrender.com"],
     })
   );
 
